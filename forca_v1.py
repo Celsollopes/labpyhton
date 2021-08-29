52de0a798ca9acfdff3ed885744eacaad1873700
clopes <camlj@documentsolutions.com.br>
2021-08-29 19:42:02 -0300

    Completa os métodos da classe

--- forca_v1.py
+++ forca_v1.py
@@ -1,98 +1,62 @@
 # Hangman Game (Jogo da Forca) 
-# Programação Orientada a Objetos
 
-# Import
 import random
-from code import forcalist as brd
+from util import forcalist as brd
 
 # Board (tabuleiro)
-board = ['''
-
->>>>>>>>>>Hangman<<<<<<<<<<
-
-+---+
-|   |
-    |
-    |
-    |
-    |
-=========''', '''
-
-+---+
-|   |
-O   |
-    |
-    |
-    |
-=========''', '''
-
-+---+
-|   |
-O   |
-|   |
-    |
-    |
-=========''', '''
-
- +---+
- |   |
- O   |
-/|   |
-     |
-     |
-=========''', '''
-
- +---+
- |   |
- O   |
-/|\  |
-     |
-     |
-=========''', '''
-
- +---+
- |   |
- O   |
-/|\  |
-/    |
-     |
-=========''', '''
-
- +---+
- |   |
- O   |
-/|\  |
-/ \  |
-     |
-=========''']
-
+board = brd.board
 
 # Classe
 class Hangman:
 
 	# Método Construtor
 	def __init__(self, word):
+		self.word = word
+		self.missed_letters = []
+		self.guessed_letter = []		
 		
-		
-	# Método para adivinhar a letra
+	# Método para adivinhar a letra 
 	def guess(self, letter):
-		
+		if letter in self.word and letter not in self.guessed_letter:
+			self.guessed_letter.append(letter)
+		elif letter not in self.word and letter not in self.missed_letters:
+			self.missed_letters.append(letter)
+		else:
+			return False
+		return True
 		
 	# Método para verificar se o jogo terminou
 	def hangman_over(self):
-		
+		return self.hangman_won() or (len(self.missed_letters) == 6)		
 		
 	# Método para verificar se o jogador venceu
 	def hangman_won(self):
-		
+		if '_' not in self.hide_word():
+			return True
+		return False
 
 	# Método para não mostrar a letra no board
 	def hide_word(self):
-		
+		rtn = ''
+		for letter in self.word:
+			if letter not in self.guessed_letter:
+				rtn += '_'
+			else:
+				rtn += letter
+		return rtn
 		
 	# Método para checar o status do game e imprimir o board na tela
 	def print_game_status(self):
-		
+		print(board[len(self.missed_letters)])
+		print('\nPalavra: ' + self.hide_word())
+		print('\nLetras erradas: ',)
+		for letter in self.missed_letters:
+			print(letter,)
+		#print()
+		print('Letras corretas: ',)
+		for letter in self.guessed_letter:
+			print(letter,)
+		#print()
 
 # Função para ler uma palavra de forma aleatória do banco de palavras
 def rand_word():
@@ -108,7 +72,10 @@ def main():
 	game = Hangman(rand_word())
 
 	# Enquanto o jogo não tiver terminado, print do status, solicita uma letra e faz a leitura do caracter
-	
+	while not game.hangman_over():
+		game.print_game_status()
+		user_input = input('\nDigite uma letra: ')
+		game.guess(user_input)
 
 	# Verifica o status do jogo
 	game.print_game_status()	
